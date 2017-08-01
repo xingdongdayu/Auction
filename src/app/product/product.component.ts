0f375885717bb09e9a61dfddaf10cfa8654d71d2
Dong <xingdongdayu@users.noreply.github.com>
2017-08-01 14:43:22 -0700

    Finish class 2

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -7,13 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
   private products: Array<Product>;
+  private imgUrl = 'http://placehold.it/320x150';
   constructor() { }
 
   ngOnInit() {
     this.products = [
       new Product(1, "First Product", 1.99, 3.5, "This is the first product.", ["Electronic", "Hardware"]),
       new Product(2, "Second Product", 2.99, 2.5, "This is the second product.", ["Game", "Software"]),
-      new Product(3, "Third Product", 3.99, 4.5, "This is the third product.", ["Electronic", "Hardware"]),
+      new Product(3, "Third Product", 3.99, 4.0, "This is the third product.", ["Electronic", "Hardware"]),
       new Product(4, "Fourth Product", 4.99, 1.5, "This is the fourth product.", ["Music", "Software"]),
       new Product(5, "Fifth Product", 5.99, 3.5, "This is the fifth product.", ["Electronic", "Hardware"]),
       new Product(6, "Sixth Product", 6.99, 2.5, "This is the sixth product.", ["Movie", "Software"])
