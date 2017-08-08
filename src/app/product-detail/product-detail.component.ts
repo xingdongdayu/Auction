edf35ab302538e5c147dc2506d83b7aa069531c8
Dong <xingdongdayu@users.noreply.github.com>
2017-08-08 00:03:07 -0700

    完善评分评论功能

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -10,6 +10,11 @@ import {Product, ProductService, Comment} from "../shared/product.service";
 export class ProductDetailComponent implements OnInit {
   product: Product;
   comments: Comment[];
+
+  newRating: number = 5;
+  newComment: string = "";
+
+  isCommentHidden = true;
   constructor(private  routeInfo: ActivatedRoute, private productService: ProductService) { }
 
   ngOnInit() {
@@ -18,4 +23,16 @@ export class ProductDetailComponent implements OnInit {
     this.comments = this.productService.getCommentsForProductId(productId);
   }
 
+  addComment() {
+    let comment = new Comment(0, this.product.id, new Date().toISOString(), "someone", this.newRating, this.newComment);
+    this.comments.unshift(comment);
+    // 重新计算平均评分
+    // reduce方法遍历comments中的每个comment，有两个参数，第一个是匿名函数，第二个是初始值。这里sum初始值是0，comment是遍历comments的每个元素。匿名函数的结果就作为下一次遍历的sum值，最后得到所有评价的评分总和
+    let sum = this.comments.reduce((sum, comment) => sum + comment.rating, 0);//
+    this.product.rating = sum / this.comments.length;
+    // 发表评论后清空评论区
+    this.newComment = null;
+    this.newRating = 5;
+    this.isCommentHidden = true;
+  }
 }
--- src/app/stars/stars.component.ts
+++ src/app/stars/stars.component.ts
@@ -1,22 +1,40 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'app-stars',
   templateUrl: './stars.component.html',
   styleUrls: ['./stars.component.css']
 })
-export class StarsComponent implements OnInit {
+export class StarsComponent implements OnInit, OnChanges {
   @Input()
   private  rating: number = 0;
+  @Output()
+  private ratingChange: EventEmitter<number> = new EventEmitter();
   private  stars: boolean[];
+  @Input()
+  private readonly: boolean = true;
   constructor() { }
 
   ngOnInit() {
     // this.stars = [false,true,true,true,true];
+    // 更新stars数组，更新星星显示的任务放到ngOnChanges里了，因为在运行ngOnInit前，ngOnChanges是会先运行一次的
+    // this.stars = [];
+    // for(let i = 1; i <= 5; i++){
+    //   this.stars.push(i > this.rating);
+    // }
+  }
+  // 当rating接到newRating的值时，显示的星星数量没有变化，这是因为stars数组没有更新。所以要用到ngOnChanges钩子，在rating值变化时，更新stars数组并更新星星显示
+  ngOnChanges(changes: SimpleChanges): void {
     this.stars = [];
     for(let i = 1; i <= 5; i++){
       this.stars.push(i > this.rating);
     }
   }
-
+  clickStar(index: number) {
+    if(!this.readonly){
+      this.rating = index + 1;
+      // this.ngOnInit();// 根据新得到的rating值，确定显示多少个实心星星，多少个空心星星 //更新星星的任务放到ngOnChange中了，这里就不用在运行ngOnInit了，因为rating值变化后，会自动运行ngOnChanges
+      this.ratingChange.emit(this.rating);
+    }
+  }
 }
